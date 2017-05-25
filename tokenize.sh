442ada875b39731bc869cf9ced849ae33e1b93dc
shuoyangd <shuoyangd@gmail.com>
2017-05-25 15:14:50 -0400

    delete tokenize.sh

--- tokenize.sh
+++
@@ -1,2 +0,0 @@
-/home/shuoyangd/mosesstd/scripts/tokenizer/tokenizer.perl -a -l en < /export/b18/mtma17/data/lc/europarl-v7.de-en.en.lc > /export/b18/mtma17/data/tokenized/europarl-v7.de-en.en.lc.tokenized
-/home/shuoyangd/mosesstd/scripts/tokenizer/tokenizer.perl -a -l de < /export/b18/mtma17/data/lc/europarl-v7.de-en.de.lc > /export/b18/mtma17/data/tokenized/europarl-v7.de-en.de.lc.tokenized
