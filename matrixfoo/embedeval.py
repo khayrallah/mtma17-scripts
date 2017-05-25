bb3a4a877525e6d7328fd475267efb5fbe1b4777
Sebastian J. Mielke <sjm@sjmielke.com>
2017-05-25 14:55:53 -0400

    Polyglot embeddings finally allow full phrase-table smoothing!

--- matrixfoo/embedeval.py
+++ matrixfoo/embedeval.py
@@ -2,121 +2,126 @@ from lshash.lshash import LSHash
 from bitarray import bitarray
 from math import cos
 import numpy as np
+from numpy.linalg import norm
 import scipy.spatial.distance
+from collections import defaultdict, Counter
 
 import os.path
 import time
 import sys
 import pickle
 
-def data_preprocessing(vocab_size = 100000):
-	with open('/mnt/hylia/embed_wiki_de_2M_200D/vocabulary.pickle', 'rb') as f:
-		o = pickle.load(f)
-		source_words = set([w for (s, w) in sorted([(s, w) for (w,s) in o.items()])][0:vocab_size])
-	print("Got vocab.", file = sys.stderr)
-
-	model = {}
-	i = 0
-	with open("/mnt/hylia/embed_wiki_de_2M_200D/embedding_file") as f:
-		l = f.readline()
-		[vocabsize, dims] = l.split()
-		[vocabsize, dims] = [int(vocabsize), int(dims)]
-		l = f.readline()
-		while l != '':
-			l = l[:-1] # remove trailing newline
-			l = l.split()
-			w = l[0]
-			if w in source_words:
-				vals = np.array([float(v) for v in l[1:]])
-				assert(len(vals) == dims)
-				model[w] = vals
-			l = f.readline() # get next one
-			i += 1
-	assert(len(model) == vocab_size)
-	print("Got embeddings.", file = sys.stderr)
-
-	planes = 10
-	lsh = LSHash(planes, dims)
-	for w in source_words:
-		lsh.index(model[w], extra_data=w)
-	#source_hashes = [bitarray(lsh._hash(lsh.uniform_planes[0], model[w])) for w in source_words]
-	print("Got hashes.", file = sys.stderr)
+def import_polyglot():
+	# Get raw word counts in polyglot corpus
+	if os.path.isfile("../embedding-matrixfoo-files/polyglot-de.wordcounts.pickle"):
+		with open("../embedding-matrixfoo-files/polyglot-de.wordcounts.pickle", 'rb') as f:
+			casecounter = pickle.load(f)
+	else:
+		casecounter = defaultdict(Counter)
+		with open('embedding-matrixfoo-files/polyglot-de.full.txt') as f:
+			for line in f.readlines():
+				for word in line.split():
+					casecounter[word.lower()][word] += 1
+		with open("../embedding-matrixfoo-files/polyglot-de.wordcounts.pickle", 'wb') as f:
+			pickle.dump(casecounter, f)
+	print("Got raw word counts.", file = sys.stderr)
 	
+	# Get list of lowercase-count-tuples
+	tups = []
+	for (lcw, d) in casecounter.items():
+		tups.append((sum(d.values()), lcw, d))
+	tups = [(w, d) for (s, w, d) in sorted(tups, reverse = True)[0:20000]]
+	vocab_set = set([w for (w, d) in tups])
+	print("Got overall word counts.", file = sys.stderr)
+	
+	# Get new dictionary of relative weights
+	caseweighter = defaultdict(dict)
+	for (lowerword, d) in casecounter.items():
+		if lowerword in vocab_set:
+			Z = float(sum(d.values()))
+			for (caseword, c) in d.items():
+				caseweighter[lowerword][caseword] = float(c) / Z
+	print("Got raw word counts.", file = sys.stderr)
+	
+	source_words = set()
+	cased_model = {}
+	dims = None
+	with open("../embedding-matrixfoo-files/polyglot-de.pkl", 'rb') as f:
+		o = pickle.load(f, encoding='latin-1')
+		for (w, e) in zip(o[0], o[1]):
+			if w.lower() in vocab_set:
+				source_words.add(w)
+				cased_model[w] = e
+				assert(dims == None or dims == len(e))
+				dims = len(e)
+	print("Got cased model.", file = sys.stderr)
+	
+	# Weighted average between casings
+	model = {}
+	for (lowerword, d) in caseweighter.items():
+		indivs = [cased_model[casedword] * weight for (casedword, weight) in d.items() if casedword in cased_model]
+		if indivs != []:
+			model[lowerword] = np.sum(indivs, axis = 0)
+			assert(len(model[lowerword]) == len(indivs[0]))
+		else:
+			print(f"No entry for »{lowerword}«")
+	print("Got lowercased model.", file = sys.stderr)
+	
+	lsh = LSHash(1024, dims)
+	for w in model.keys():
+		lsh.index(model[w], extra_data=w)
+	print("Got hashed.", file = sys.stderr)
+	
+	print("Got all polyglot!", file = sys.stderr)
 	return (source_words, model, lsh)
 
 def nns_hash(w):
-	return [(sim, word) for ((vals, word), sim) in lsh.query(model[w], distance_func = "cosine")][0:5]
+	h1 = bitarray(lsh._hash(lsh.uniform_planes[0], model[w]))
+	cands = []
+	for w2w in model.keys():
+		h2 = bitarray(lsh._hash(lsh.uniform_planes[0], model[w2w]))
+		s = cos(3.141592653589 * float((h1 ^ h2).count()) / float(1024))
+		cands.append((s, w2w))
+	return sorted(cands, reverse=True)[:5]
 
 def nns_truecosin(w):
 	w1 = model[w]
 	cands = []
-	for w2w in source_words:
+	for w2w in model.keys():
 		w2 = model[w2w]
 		#cands.append((1.0 - scipy.spatial.distance.euclidean(w1, w2), w2w))
 		cands.append((1.0 - scipy.spatial.distance.cosine(w1, w2), w2w))
 	return sorted(cands, reverse=True)[:5]
 
 def printnns(w):
-	print(f"» {w}")
-	l = nns_truecosin(w)
-	print("\n".join([f"{s:4.4f} {w}" for (s,w) in l]))
+	if w in model:
+		print(f"» {w}")
+		l = nns_truecosin(w)
+		print("\n".join([f"{s:4.4f} {w}" for (s,w) in l]))
 
-
-
-if os.path.isfile("/tmp/embmodel.pickle"):
-	with open("/tmp/embmodel.pickle", 'rb') as f:
+if os.path.isfile("../embedding-matrixfoo-files/polyglot-de-embmodel.pickle"):
+	with open("../embedding-matrixfoo-files/polyglot-de-embmodel.pickle", 'rb') as f:
 		(source_words, model, lsh) = pickle.load(f)
 else:
-	(source_words, model, lsh) = data_preprocessing()
-	with open("/tmp/embmodel.pickle", 'wb') as f:
+	(source_words, model, lsh) = import_polyglot()
+	with open("../embedding-matrixfoo-files/polyglot-de-embmodel.pickle", 'wb') as f:
 		pickle.dump((source_words, model, lsh), f)
 
-
-printnns('frosch')
 printnns('deutschland')
-printnns('deutsch')
-printnns('dann')
-printnns('blau')
-printnns('weg')
-printnns('straße')
-exit(0)
 
-def simi(w1, w2):
-	if w1 not in model or w2 not in model:
-		print(f"{w1} or {w2} not in model")
-	else:
-		h1 = bitarray(lsh._hash(lsh.uniform_planes[0], model[w1]))
-		h2 = bitarray(lsh._hash(lsh.uniform_planes[0], model[w2]))
-		s_h = cos(3.141592653589 * float((h1 ^ h2).count()) / float(planes))
-		s_w = 1 - cosine(model[w1], model[w2])
-		print(f"{w1} {w2} = {s_h:.4f} hash {s_w:.4f} word => {abs(s_h-s_w):.4f} diff")
+X_labels = sorted(list(model.keys()))
+X_vals   = np.array([model[w] for w in X_labels])
 
-simi("obwohl", "trotzdem")
-simi("denn", "weil")
-simi("dann", "danach")
+cosmatrix = 1.0 - scipy.spatial.distance.pdist(X_vals, metric='cosine')
+cosmatrix = scipy.spatial.distance.squareform(cosmatrix)
+cosmatrix += np.eye(len(X_labels)) # dunno why this is necessary...
 
-simi("tasse", "becher")
-simi("brot", "brötchen")
-simi("schwarzbrot", "graubrot")
-simi("kessel", "topf")
+print("» deutschland")
+print(cosmatrix[X_labels.index("deutschland")])
+deu_sims = list(cosmatrix[X_labels.index("deutschland")])
+for (s, w) in sorted(list(zip(deu_sims, X_labels)), reverse = True)[0:5]:
+	print(f"{s:.4f} {w}")
 
-simi("pfad", "straße")
-simi("könig", "kaiser")
-simi("könig", "führer")
-simi("blau", "grün")
-simi("blau", "rot")
-simi("blau", "gelb")
-simi("blau", "scharf")
-simi("blau", "stumpf")
-
-print("Starting measurements")
-for i in range(1):
-	start = time.time()
-	for (w1, h1) in zip(source_words, source_hashes):
-		for (w2, h2) in zip(source_words, source_hashes):
-			#s_w = 1 - cosine(model[w1], model[w2])
-			s_h_form = cos(3.141592653589 * float((h1 ^ h2).count()) / float(planes))
-			#print(abs(s_h_form - s_w))
-	end = time.time()
-
-	print(f"gensim: {end - start}")
+# Cool. Save it for matrixfoo.py.
+with open("../embedding-matrixfoo-files/polyglot-de-cosmatrix.pickle", 'wb') as f:
+	pickle.dump((X_labels, cosmatrix), f)
--- matrixfoo/matrixfoo.py
+++ matrixfoo/matrixfoo.py
@@ -1,5 +1,9 @@
 import numpy as np
 import time
+import pickle
+import shutil
+import os
+import os.path
 
 def get_toy_matrices():
     vocab_size_s = 4
@@ -39,25 +43,104 @@ def get_random_matrices(size):
     translation_matrix = np.random.rand(size, size)
     return (translation_matrix, similarity_matrix)
 
-def crunch_matrices(translation_matrix, similarity_matrix):
-    # Global normalize (useless?)
-    #translation_matrix /= translation_matrix.sum()
-    similarity_matrix /= similarity_matrix.sum(axis=1)[:, np.newaxis]
+def get_similarity_matrix():
+    with open("../embedding-matrixfoo-files/polyglot-de-cosmatrix.pickle", 'rb') as f:
+        (X_labels, cosmatrix) = pickle.load(f)
+    return (X_labels, cosmatrix)
 
-    for i in range(5):
-        print(f"Iter {i}: ", end='')
-        start = time.time()
-        translation_matrix = np.dot(similarity_matrix, translation_matrix)
-        #translation_matrix /= translation_matrix.sum()
-        print(translation_matrix.sum(), end='')
-        #print(translation_matrix)
-        end = time.time()
-        print(f" ({end - start} s)")
+def get_translation_matrix(X_labels):
+    srcdict = {l: i for (i, l) in enumerate(X_labels)}
+    ptpath = "../embedding-matrixfoo-files/lexicon.e2f.de-en.200000.pruned-a+e-n3"
+    # populate target vocab dict
+    trgdict = {}
+    trgdict_rev = {}
+    with open(ptpath, 'r', encoding='utf-8') as f:
+        i = 0
+        for line in f.read().splitlines():
+            l = line.split(" ||| ")
+            target = l[1]
+            if target not in trgdict:
+                trgdict[target] = i
+                trgdict_rev[i] = target
+                i += 1
+    # construct matrix
+    transmatrix = np.zeros((len(srcdict), len(trgdict)))
+    with open(ptpath, 'r', encoding='utf-8') as f:
+        accept = 0
+        discard = 0
+        for line in f.read().splitlines():
+            l = line.split(" ||| ")
+            source = l[0]
+            target = l[1]
+            score = l[2]
+            if source in srcdict:
+                transmatrix[srcdict[source]][trgdict[target]] = score
+                accept += 1
+            else:
+                #print("Discarded line:", line)
+                discard += 1
+    print(f"Read {accept} useful lines and {discard} lines whose source was not accepted.")
+    return (trgdict_rev, transmatrix)
 
+pt_export_threshold = 0.001
+presoftmax_multiplier = 80.0
+softmax = True
 
-print("Generating matrices: ", end='')
+def export_phrase_table(filename, X_labels, trgdict_rev, translation_matrix):
+    with open(filename, 'w') as f:
+        for (w1, transrow) in zip(X_labels, translation_matrix):
+            for (i, score) in enumerate(transrow):
+                if score > pt_export_threshold:
+                    w2 = trgdict_rev[i]
+                    print(f"{w1} ||| {w2} ||| {score:.7f} ||| ||| ", file = f)
+
+# Get similarity matrix
+(X_labels, cosmatrix) = get_similarity_matrix()
 start = time.time()
-t, s = get_random_matrices(2000)
+# Softmax similarity matrix
+if softmax:
+    newmat = np.zeros_like(cosmatrix)
+    cosmatrix *= presoftmax_multiplier
+    for i in range(len(cosmatrix)):
+        Z = np.sum(np.exp(cosmatrix[i]))
+        newmat[i] = np.exp(cosmatrix[i]) / Z
+    cosmatrix = newmat
 end = time.time()
-print(f"{end - start} s")
-crunch_matrices(t, s)
+print(f"Similarity matrix softmaxing took {end - start} s")
+# Get translation matrix
+(trgdict_rev, transmatrix) = get_translation_matrix(X_labels)
+
+def translatable_stats(X_labels, transmatrix):
+    words_okay = [w for w, row in zip(X_labels, transmatrix) if np.count_nonzero(row) > 0]
+    words_nope = [w for w, row in zip(X_labels, transmatrix) if np.count_nonzero(row) == 0]
+    res = f"{100.0 * len(words_okay) / len(X_labels):.2f}% translatable, {100.0 * len(words_nope) / len(X_labels):2f}% untranslatable\n"
+    words_okay = [w for w, row in zip(X_labels, transmatrix) if np.count_nonzero(row > pt_export_threshold) > 0]
+    words_nope = [w for w, row in zip(X_labels, transmatrix) if np.count_nonzero(row > pt_export_threshold) == 0]
+    pt_export_threshold
+    res += f"{100.0 * len(words_okay) / len(X_labels):.2f}% in phrasetab, {100.0 * len(words_nope) / len(X_labels):2f}% not in phrasetab"
+    return res
+
+
+dirname = "../embfooruns/" + (f"softmax-{presoftmax_multiplier}-" if softmax else "") + f"prune-below-{pt_export_threshold}"
+
+if os.path.isdir(dirname):
+    shutil.rmtree(dirname)
+os.mkdir(dirname)
+
+with open(dirname + "/iterations.log", 'w') as lf:
+    # Save it (sanity check)
+    export_phrase_table(dirname + "/pt.0", X_labels, trgdict_rev, transmatrix)
+    print(translatable_stats(X_labels, transmatrix), file = lf)
+    #print("Transmatrix sum:", transmatrix.sum())
+
+    for i in range(30):
+        print(f"Iter {i+1}: ", end='')
+        start = time.time()
+        transmatrix = np.dot(cosmatrix, transmatrix)
+        #transmatrix /= transmatrix.sum()
+        print(transmatrix.sum(), end='')
+        #print(transmatrix)
+        end = time.time()
+        export_phrase_table(dirname + f"/pt.{i+1}", X_labels, trgdict_rev, transmatrix)
+        print(f" ({end - start} s)")
+        print(translatable_stats(X_labels, transmatrix), file = lf)
